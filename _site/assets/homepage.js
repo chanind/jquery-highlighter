31d25ebead2c6aa761188819967ef87f956d33d7
David Chanin <dchanin@quizlet.com>
2013-03-03 14:25:30 -0800

    fixing up the inline-highlighting on the homepage

--- _site/assets/homepage.js
+++ _site/assets/homepage.js
@@ -3,12 +3,30 @@
 
   Homepage = (function() {
 
-    Homepage.prototype.ranges = [[3, 40], [37, 100], [80, 140]];
+    Homepage.prototype.ranges = [[27, 52], [68, 97], [152, 166], [180, 193]];
 
     function Homepage() {
       _.bindAll(this);
-      alert('here');
+      this.currentRangeIndex = 0;
+      this.nextHighlight();
+      this.highlightTimer = setInterval(this.nextHighlight, 3000);
     }
+
+    Homepage.prototype.nextHighlight = function() {
+      var $lastHighlight, range;
+      range = this.ranges[this.currentRangeIndex];
+      $('.highlight').removeClass('highlight');
+      $lastHighlight = $('.lead').highlighter('markHighlight', range, {
+        highlightClass: 'marker'
+      });
+      setTimeout(function() {
+        return $lastHighlight.addClass('highlight');
+      }, 500);
+      this.currentRangeIndex += 1;
+      if (this.currentRangeIndex >= this.ranges.length) {
+        return this.currentRangeIndex = 0;
+      }
+    };
 
     return Homepage;
 
--- assets/homepage.coffee
+++ assets/homepage.coffee
@@ -3,13 +3,26 @@
 class Homepage
 
 	ranges: [
-		[3, 40]
-		[37, 100]
-		[80, 140]
+		[27, 52]
+		[68, 97]
+		[152, 166]
+		[180, 193]
 	]
 
 	constructor: ->
 		_.bindAll(@)
-		alert('here')
+		@currentRangeIndex = 0
+		@nextHighlight()
+		@highlightTimer = setInterval @nextHighlight, 3000
+
+	nextHighlight: ->
+		range = @ranges[@currentRangeIndex]
+		$('.highlight').removeClass('highlight')
+		$lastHighlight = $('.lead').highlighter('markHighlight', range, {highlightClass: 'marker'})
+		setTimeout ->
+			$lastHighlight.addClass('highlight')
+		, 500
+		@currentRangeIndex += 1
+		@currentRangeIndex = 0 if @currentRangeIndex >= @ranges.length
 
 window.homepage = new Homepage()
